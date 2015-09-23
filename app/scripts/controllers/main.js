f907537ee66ff33ee610358f15f8fd2e3b9988de
Abdullah Al Mamun <am@selise.ch>
2015-09-23 16:23:31 +0600

    sticky nav set

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,63 @@
  * # MainCtrl
  * Controller of the firstThemeOnAppLandingApp
  */
-angular.module('firstThemeOnAppLandingApp')
-  .controller('MainCtrl', function ($scope) {
+
+LandApp.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+
+
+        /* =================================
+         ===  STICKY NAV                 ====
+         =================================== */
+
+        $(document).ready(function() {
+            $('.main-navigation').onePageNav({
+                scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
+                filter: ':not(.external)',
+                changeHash: true
+            });
+
+        });
+
+    /* COLLAPSE NAVIGATION ON MOBILE AFTER CLICKING ON LINK - ADDED ON V1.5*/
+
+/*    if (matchMedia('(max-width: 480px)').matches) {
+        $('.main-navigation a').on('click', function () {
+            $(".navbar-toggle").click();
+        });
+    }*/
+
+
+    /* NAVIGATION VISIBLE ON SCROLL */
+
+    $(document).ready(function () {
+        mainNav();
+    });
+
+    $(window).scroll(function () {
+        mainNav();
+    });
+
+//    if (matchMedia('(min-width: 992px), (max-width: 767px)').matches) {
+        function mainNav() {
+            var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+            if (top >40) $('.sticky-navigation').stop().animate({"top": '0'});
+
+            else $('.sticky-navigation').stop().animate({"top": '-60'});
+        }
+//    }
+
+/*    if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
+        function mainNav() {
+            var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+            if (top > 40) $('.sticky-navigation').stop().animate({"top": '0'});
+
+            else $('.sticky-navigation').stop().animate({"top": '-120'});
+        }
+    }*/
   });
